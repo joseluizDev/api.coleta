7be0415ce5c42d90b94ecb8df394674c02c37d1a
Jose luiz <luizzzz1996@gmail.com>
2025-11-01 07:58:07 -0300

    fix: Add missing PontoColetadoRepository parameter to RelatorioService test constructor

--- Tests/RelatorioServiceTests.cs
+++ Tests/RelatorioServiceTests.cs
@@ -17,8 +17,9 @@ public class RelatorioServiceTests
     {
         var repository = new RelatorioRepository(context);
         var minioStorage = new FakeMinioStorage();
+        var pontoColetadoRepository = new PontoColetadoRepository(context);
         var unitOfWork = new UnitOfWorkImplements(context);
-        return new RelatorioService(repository, minioStorage, unitOfWork);
+        return new RelatorioService(repository, minioStorage, pontoColetadoRepository, unitOfWork);
     }
 
     [Fact]
